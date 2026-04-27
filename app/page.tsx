f9749816364041a011c4676d61c98ef96d7e1811
vgandia13 <victorxd1324@gmail.com>
2026-04-27 10:17:26 +0200

    feat: add LoginPage component and Card UI elements for user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
+    redirect("/pages/login");
     return (
       <div className="bg-slate-500">
-        
+              
       </div>
     );
 }
---
+++ app/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+
+const LoginPage = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Iniciar Sesión</CardTitle>
+                <CardDescription>Inicie sesión a su cuenta</CardDescription>
+            </CardHeader>
+        </Card>
+    );
+}
+
+export default LoginPage
