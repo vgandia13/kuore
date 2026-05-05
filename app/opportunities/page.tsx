201e1c764198e55b3754b667d94d22bba0365293
vgandia13 <victorxd1324@gmail.com>
2026-05-05 09:22:07 +0200

    fix: wrap OpportunitiesCard in a flex container for better alignment

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -2,12 +2,14 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card"
 
 const OpportunitiesPage = () => {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Opportunities</CardTitle>
-            </CardHeader>
+      <div className="flex items-center justify-center">
+        <Card className="size-48">
+          <CardHeader>
+            <CardTitle>Opportunities</CardTitle>
+          </CardHeader>
         </Card>
-    )
+      </div>
+    );
 }
 
 export default OpportunitiesPage;
