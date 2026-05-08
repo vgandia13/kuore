1ca3b2f587f09a7acd38f37f39f6ea786a9d34a5
vgandia13 <victorxd1324@gmail.com>
2026-05-08 13:51:32 +0200

    feat: add PieSeries and implement donut chart in GraphArea component

--- app/home/GraphArea.tsx
+++ app/home/GraphArea.tsx
@@ -1,6 +1,7 @@
 import { Chart, Title, Legend } from "@highcharts/react";
 import { AreaSeries } from "@highcharts/react/series/Area";
 import { ColumnSeries } from "@highcharts/react/series/Column";
+import { PieSeries } from "@highcharts/react/series/Pie";
 
 import { useTheme } from "next-themes";
 
@@ -12,7 +13,8 @@ const GraphArea = () => {
 
   return (
     <>
-      /* Grafico de area */
+      {/* Grafico de area */}
+
       <Chart
         title="Grafica de area"
         spacing={[20, 20, 25, 20]}
@@ -85,48 +87,98 @@ const GraphArea = () => {
       >
         <AreaSeries data={[3, 4, 3, 5, 2, 4, 2, 3, 2, 5, 1.75, 3, 4, 3]} />
       </Chart>
-      /* Grafico de columnas */
-      <Chart title="Grafico de columnas stacked" options={{
-        chart: {
-          type: "column",
-          backgroundColor: theme === "dark" ? "#000000" : "#ffffff",
-          style: {
-            fontFamily: "inherit",
-            color: theme === "dark" ? "#ffffff" : "#000000", // afecta bastantes elementos
+
+      {/* Grafico de columnas */}
+
+      <Chart
+        title="Grafico de columnas stacked"
+        options={{
+          chart: {
+            type: "column",
+            backgroundColor: theme === "dark" ? "#000000" : "#ffffff",
+            style: {
+              fontFamily: "inherit",
+              color: theme === "dark" ? "#ffffff" : "#000000", // afecta bastantes elementos
+            },
           },
-        },
-        xAxis: {
-          labels: {
-            style: { color: theme === "dark" ? "#ffffff" : "#000000" },
+          xAxis: {
+            labels: {
+              style: { color: theme === "dark" ? "#ffffff" : "#000000" },
+            },
+            lineColor: theme === "dark" ? "#555" : "#ccd6eb",
+            tickColor: theme === "dark" ? "#555" : "#ccd6eb",
           },
-          lineColor: theme === "dark" ? "#555" : "#ccd6eb",
-          tickColor: theme === "dark" ? "#555" : "#ccd6eb",
-        },
-        yAxis: {
-          labels: {
-            style: { color: theme === "dark" ? "#ffffff" : "#000000" },
+          yAxis: {
+            labels: {
+              style: { color: theme === "dark" ? "#ffffff" : "#000000" },
+            },
           },
-        },
-        plotOptions: {
-          column: {
-            stacking: "normal",
-          }  
-        },
-        series: [{
-          name: "John",
-          data: [3, 4, 3, 5, 2, 4, 2, 3,]
-        },
-        {
-          name: "Jane",
-          data: [2, 3, 2, 4, 1, 3, 1, 2,]
-        },
-        {
-          name: "Joe",
-          data: [1, 2, 1, 3, 0.5, 2, 5, 1,]
-        },
-    ]
-      }}>
+          plotOptions: {
+            column: {
+              stacking: "normal",
+            },
+          },
+          series: [
+            {
+              name: "John",
+              data: [3, 4, 3, 5, 2, 4, 2, 3],
+            },
+            {
+              name: "Jane",
+              data: [2, 3, 2, 4, 1, 3, 1, 2],
+            },
+            {
+              name: "Joe",
+              data: [1, 2, 1, 3, 0.5, 2, 5, 1],
+            },
+          ],
+        }}
+      >
         <ColumnSeries />
+      </Chart>
+
+      {/* Grafico de donut */}
+      {/*TODO: investigar como cambiar de pie a donut */}
+      <Chart
+        title="Grafico de donut"
+        options={{
+          chart: {
+            type: "pie",
+            backgroundColor: theme === "dark" ? "#000000" : "#ffffff",
+            style: {
+              fontFamily: "inherit",
+              color: theme === "dark" ? "#ffffff" : "#000000", // afecta bastantes elementos
+            },
+          },
+          legend: {
+            itemStyle: { color: theme === "dark" ? "#ffffff" : "#000000" },
+          },
+          plotOptions: {
+            pie: {},
+          },
+          series: [
+            {
+              dataLabels: [
+                {
+                  enabled: true,
+                  distance: 20,
+                },
+              ],
+              data: [
+                {
+                  name: "John",
+                  y: 5,
+                },
+                {
+                  name: "Jane",
+                  y: 3,
+                },
+              ],
+            },
+          ],
+        }}
+      >
+        <PieSeries />
       </Chart>
     </>
   );
